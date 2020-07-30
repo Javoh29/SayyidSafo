b60a12f6453e2e911fc4e3f596d0c013c921d31d
Javoh29 <33982593+Javoh29@users.noreply.github.com>
2020-07-30 18:05:33 +0500

    update

---
+++ app/src/main/java/uz/mnsh/sayyidsafo/ui/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package uz.mnsh.sayyidsafo.ui.activity
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import uz.mnsh.sayyidsafo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme_NoActionBar)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+    }
+}
